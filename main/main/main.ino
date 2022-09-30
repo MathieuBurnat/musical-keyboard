012ca377d8b5b927d522d903d72e36758e2c9559
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-09-30 16:29:38 +0200

    Great, I played au clair de la lune

--- main/main/main.ino
+++ main/main/main.ino
@@ -2,8 +2,8 @@ const int piezoPin = 8;
 
 int buttons[6] = {2};
 
-// C, D, E, F || Aka Do, Ré, Mi, FA in french
-int notes[] = {262, 294, 339, 349};
+// G, A, E, F || Aka Sol, La, Si in french
+int notes[] = {196, 220, 330, 349};
 
 void setup() {
   Serial.begin(9600);
@@ -11,17 +11,18 @@ void setup() {
 
 void loop() {
   int keyValue = analogRead(A0);
-  Serial.print(keyValue);
 
   if(keyValue == 1023){
-    tone(piezoPin, notes[0]);
-  }else if (keyValue >= 990 && keyValue <= 1010){
-    tone(piezoPin, notes[1]);
-  }else if (keyValue >= 505 && keyValue <= 515){
-    tone(piezoPin, notes[2]);
-  }else if (keyValue >= 5 && keyValue <= 10){
     tone(piezoPin, notes[3]);
+  }else if (keyValue >= 990 && keyValue <= 1010){
+    tone(piezoPin, notes[2]);
+  }else if (keyValue >= 505 && keyValue <= 515){
+    tone(piezoPin, notes[1]);
+  }else if (keyValue >= 5 && keyValue <= 10){
+    tone(piezoPin, notes[0]);
   }else{
     noTone(8);
   }
+
+  delay(10);
 }
